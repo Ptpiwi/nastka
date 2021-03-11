7f40ac2572683e44b8f7cc4a095c00208fd6e883
Wojciech Kulesza <Ptpiwi@gmail.com>
2021-03-11 18:52:01 +0100

    Initial commit

---
+++ src/main/java/pl/mowk/nastka/Main.java
@@ -0,0 +1,9 @@
+package pl.mowk.nastka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(args);
+    }
+}
+
---
+++ src/main/java/pl/mowk/nastka/State.java
@@ -0,0 +1,4 @@
+package pl.mowk.nastka;
+
+public class State {
+}
